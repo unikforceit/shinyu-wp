40ed70a658f2ad5a670e7fcba06bec9323b18f99
unikforceit <freelancerakibul@gmail.com>
2023-03-28 11:23:41 -0400

    search replace and elementor support done

--- archive-news.php
+++ archive-news.php
@@ -1,6 +1,25 @@
 
 <?php get_header(); ?>
-  <section class="search-bar is-relative"><search-bar></search-bar></section>
+<section class="search-bar is-relative custom-global-search">
+    <div class="search-box-item">
+        <div class="container">
+            <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <div class="autocomplete control">
+                    <div class="control is-medium is-clearfix">
+                        <div class="columns">
+                            <div class="column is-10">
+                                <input type="search" class="search-field input is-medium" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder', 'your-text-domain' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+                            </div>
+                            <div class="column is-2">
+                                <button type="submit" class="search-submit button is-medium is-danger"><span><?php echo _x( 'Search', 'submit button', 'shyinuaddons' ); ?></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
   <section class="page-header mb-0">
     <div class="container">
       <div class="columns m-0">
--- template-contact-form.php
+++ template-contact-form.php
@@ -3,20 +3,9 @@
 /**
  * Template Name: Contact Form
  */
-get_header(); ?>
-  <section class="search-bar is-relative"><search-bar></search-bar></section>
-  <section class="page-header mb-0">
-    <div class="container">
-      <h1 class="page-title"><?php the_title(); ?></h1>
-      <?php woocommerce_breadcrumb(); ?>
-    </div>
-  </section><!-- .page-header -->
-	<main class="main-content">
-    <div class="container">
-      <div class="contact-form"><contact-form></contact-form></div>
-    </div>
-	</main>
-<?php get_footer(); ?>
+get_header();
+the_content();
+get_footer(); ?>
 
 <?php
 
